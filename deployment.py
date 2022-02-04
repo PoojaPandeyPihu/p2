8568b5c0f94babeb954e5b662111da4d18b0ccb6
Pooja Pandey <Pandey.pooja42@gmail.com>
2022-02-04 21:07:53 +0530

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -2,7 +2,7 @@
 """
 Created on Sat Jan 29 19:37:40 2022
 
-@author: vinis
+@author: pihu
 """
 
 # -*- coding: utf-8 -*-
@@ -84,11 +84,13 @@ if __name__=='__main__':
 ################################# SIDE BAR FOOTER CONTENT ############################################
 st.sidebar.title("**About**")  ######### ABOUT Section
 
-st.sidebar.header("Guided by:-")
-st.sidebar.title("*****")
+st.sidebar.header("Guided by:- Mr. Varun")
+#st.sidebar.title("")
 st.sidebar.title("Made With Streamlit by")
 st.sidebar.image("streamlitlogo1.png", width=180)    ####  Displaying streamlit logo
 st.sidebar.header("P-88 Group 5:")
 st.sidebar.write("***Priyanka***" ,"," ,"***Prithvi***")
 st.sidebar.write("***Gurpinder***" ,"," ,"***Pooja***")
 st.sidebar.write("***Vinay***" ,"," ,"***Shivani***")
+st.sidebar.write("***Anand***")
+
